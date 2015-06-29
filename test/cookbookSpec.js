6b540a82a806375691f5425b90e56e2ba1cda1f0
gvit00702 <gvit00702@gmail.com>
2015-06-29 12:58:42 +0800

    Moved the inject logic out of the it section

--- test/cookbookSpec.js
+++ test/cookbookSpec.js
@@ -1,11 +1,25 @@
-describe('MainCtrl', function(){
-	beforeEach(module('cookbook'));
+// describe('MainCtrl', function() {
+//     beforeEach(module('cookbook'));
 
-	it('should assign the correct rapper to scope', inject(function($controller, $rootScope){
-		var $scope = $rootScope.$new();
-		$controller('MainCtrl',{
-			$scope:$scope
-		});
-		expect($scope.emcee).toEqual('Tan Lee Kiong');
-	}));
-})
+//     it('should assign the correct rapper to scope', inject(function($controller, $rootScope) {
+//         var scope = $rootScope.$new();
+//         $controller('MainCtrl', {
+//             $scope: scope
+//         });
+//         expect(scope.emcee).toEqual('Tan Lee Kiong');
+//     }));
+// })
+
+describe('MainCtrl', function() {
+    var mainCtrl, scope;
+    beforeEach(module('cookbook'));
+    beforeEach(inject(function($controller, $rootScope) {
+        scope = $rootScope.$new();
+        $controller('MainCtrl', {
+            $scope: scope
+        });
+    }));
+    it('should assign the correct rapper to scope', function() {
+        expect(scope.emcee).toEqual('Tan Lee Kiong');
+    });
+});
